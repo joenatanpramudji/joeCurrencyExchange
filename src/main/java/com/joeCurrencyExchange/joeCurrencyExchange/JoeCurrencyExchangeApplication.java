61bd83efbac1592dc14118f849ac97ea9dc82c75
pramudjijoenatan <133725485+pramudjijoenatan@users.noreply.github.com>
2025-05-31 20:49:53 +0800

    Initial commit

---
+++ src/main/java/com/joeCurrencyExchange/joeCurrencyExchange/JoeCurrencyExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.joeCurrencyExchange.joeCurrencyExchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoeCurrencyExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoeCurrencyExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joeCurrencyExchange/joeCurrencyExchange/JoeCurrencyExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joeCurrencyExchange.joeCurrencyExchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoeCurrencyExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
